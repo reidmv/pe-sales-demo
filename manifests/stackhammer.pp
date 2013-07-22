a6b10098c9859776c88208f598f2190f2bf5313a
reidmv <reid@puppetlabs.com>
2013-07-22 15:01:03 +0000

    pe-sales-demo was configured

--- manifests/stackhammer.pp
+++ manifests/stackhammer.pp
@@ -5,3 +5,35 @@ node centos63001 {
 node centos63002 {
   include 'agent'
 }
+
+node centos63003 {
+  include 'agent'
+}
+
+node centos63004 {
+  include 'agent'
+}
+
+node centos63005 {
+  include 'agent'
+}
+
+node centos63006 {
+  include 'agent'
+}
+
+node centos63007 {
+  include 'agent'
+}
+
+node centos63008 {
+  include 'agent'
+}
+
+node centos63009 {
+  include 'agent'
+}
+
+node centos63010 {
+  include 'agent'
+}
